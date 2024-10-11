24fb9c16f43332ad32857c2d0d5a7d34aae5bee5
이준희 <0210leejun@gmail.com>
2024-10-11 16:39:42 +0900

    Chapter 3. Styled-Components, React Router, API 통신

--- 3주차_Chapter 3. Styled-Components, React Router, API 통신/mission1/src/pages/moviesPage.jsx
+++ 3주차_Chapter 3. Styled-Components, React Router, API 통신/mission1/src/pages/moviesPage.jsx
@@ -15,38 +15,36 @@ function MoviesPage() {
   const location = useLocation().pathname.split('/').pop();
 
   useEffect(() => {
+    let url = "";
     const getMovies = async () => {
       switch (location) {
         case 'now_playing':
           setTitle('상영 중인 영화');
-          setUrl('/movie/now_playing?language=ko-KR&page=1');
+          url = '/movie/now_playing?language=ko-KR&page=1';
           break;
         case 'popular':
           setTitle('인기있는 영화')
-          setUrl('/movie/popular?language=ko-KR&page=1');
+          url = '/movie/popular?language=ko-KR&page=1';
           break;
         case 'top_rated':
           setTitle('높은 평가를 받은 영화')
-          setUrl('/movie/top_rated?language=ko-KR&page=1');
+          url = '/movie/top_rated?language=ko-KR&page=1';
           break;
         case 'upcoming':
           setTitle('개봉 예정 중인 영화')
-          setUrl('/movie/upcoming?language=ko-KR&page=1');
+          url = '/movie/upcoming?language=ko-KR&page=1';
           break;
       }
-
-      if (!url) return;           // setUrl 하기 전 (switch문 돌기 전) axios 요청 방지
-                                  // 일단 해결은 했는데 메모리(?) 문제는 없는건가?
 
       const movies = await axios.get(import.meta.env.VITE_MOVIE_API_URL + url, {
         headers: {
           Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
         },
-      })
+      });
       setMovies(movies);
     };
     getMovies();
-  }, [url, location, title]);
+  }, []);
 
   return (
     <S.ContentContainer>
