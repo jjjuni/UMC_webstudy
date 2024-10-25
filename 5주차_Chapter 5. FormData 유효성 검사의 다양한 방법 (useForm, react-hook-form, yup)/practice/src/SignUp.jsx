961c48e0b5501fb8f55b80fd67654a756b41c5aa
이준희 <0210leejun@gmail.com>
2024-10-25 19:46:38 +0900

    Chapter 5. FormData 유효성 검사의 다양한 방법 (useForm, react-hook-form, yup)

--- 5주차_Chapter 5. FormData 유효성 검사의 다양한 방법 (useForm, react-hook-form, yup)/practice/src/SignUp.jsx
+++ 5주차_Chapter 5. FormData 유효성 검사의 다양한 방법 (useForm, react-hook-form, yup)/practice/src/SignUp.jsx
@@ -3,13 +3,18 @@ import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 
 const SignUp = () => {
+
   const schema = yup.object().shape({
-    email: yup.string().email('이메일 형식에 맞춰주세요').required('이메일을 입력해주세요'),
-    password: yup.string().min(8, '비밀번호는 8자 이상이어야 합니다.').max(16, '비밀번호는 16자 이하여야 합니다').required('비밀번호를 입력해주세요'),
+    email: yup.string().required('empty').email('이메일 형식에 맞춰주세요'),
+    password: yup.string().required('empty').min(8, '비밀번호는 8자 이상이어야 합니다.').max(16, '비밀번호는 16자 이하여야 합니다'),
+    passwordCheck: yup.string().required('empty').oneOf([yup.ref('password'), null], '불일치'),
   })
 
-  const {register, handleSubmit, formState: {errors}} = useForm({
-    resolver: yupResolver(schema)
+  const { register, handleSubmit, watch, formState: { errors, isValid } } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onChange'
+    // value가 바뀔 때 마다 검사한다면 onChange
+    // unFocus 될 때 검사한다면 onBlur
   });
 
   const onSubmit = (data) => {
@@ -17,13 +22,50 @@ const SignUp = () => {
     console.log(data)
   }
 
+  const emailValue = watch('email');
+  const passwordValue = watch('password');
+  const passwordCheckValue = watch('passwordCheck');
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type={'email'} {...register("email")}/>
-      <p style = {{color: 'red'}}>{errors.email?.message}</p>
-      <input type={'password'} {...register("password")}/>
-      <p style = {{color: 'red'}}>{errors.password?.message}</p>
-      <input type={'submit'}/>
+      <input type="text" {...register("email")}/>
+      <div>
+          { emailValue && 
+            (errors.email? 
+              <img src='/src/icon/x_circle.svg'/>
+            :     // 유효성 불만족
+              <img src='/src/icon/check_circle.svg'/>
+                  // 유효성 만족
+            )
+          }
+      </div>
+      <input type='password' {...register("password")} />
+      <div>
+          { passwordValue && 
+            (errors.password? 
+              <img src='/src/icon/x_circle.svg'/>
+            :     // 유효성 불만족
+              <img src='/src/icon/check_circle.svg'/>
+                  // 유효성 만족
+            )
+          }
+      </div>
+      <input type='password' {...register("passwordCheck")} />
+      <div>
+          { passwordCheckValue && 
+            (passwordCheckValue !== passwordValue? 
+              <img src='/src/icon/x_circle.svg'/>
+            :     // 유효성 불만족
+              <img src='/src/icon/check_circle.svg'/>
+                  // 유효성 만족
+            )
+          }
+      </div>
+      <input 
+        type='submit'
+        disabled={!isValid}
+        
+      />
     </form>
   )
 }
