7560de3a60874877b32565b9c6dca3a75c1c0d0b
jjjuni <0210leejun@gmail.com>
2024-11-03 21:07:36 +0900

    UMC-7th-WATCHA-BE

--- UMC-7th-WATCHA-BE/test.sql
+++ UMC-7th-WATCHA-BE/test.sql
@@ -1,3 +1,3 @@
--- DELETE From user;
--- DELETE FROM sqlite_sequence WHERE name = 'user';
+-- DELETE From user;                                    -- 테이블 초기화
+-- DELETE FROM sqlite_sequence WHERE name = 'user';     -- 테이블 id 초기화 (id 1 부터 다시 시작)
 SELECT * FROM user;
