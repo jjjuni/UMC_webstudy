fd2aec9481f2fa514be6e430c84cfbb52a123fa7
jjjuni <0210leejun@gmail.com>
2024-11-15 18:04:19 +0900

    Chapter 7.  Tanstack Query (useQuery, useInfiniteQuery), 무한 스크롤, 페이지네이션

---
+++ 7주차_Chapter 7.  Tanstack Query (useQuery, useInfiniteQuery), 무한 스크롤, 페이지네이션/mission/src/App.jsx
@@ -0,0 +1,74 @@
+import HomePage from "./pages/homePage.jsx";
+import NotFound from "./pages/not-found.jsx";
+import LoginPage from "./pages/sign/loginPage.jsx";
+import SignUpPage from "./pages/sign/signupPage.jsx";
+import SearchPage from "./pages/searchPage.jsx";
+import MovieCategoryPage from "./pages/movie/movieCategoryPage.jsx";
+import MoviesPage from "./pages/movie/moviesPage_InfinityScroll.jsx";
+import MoviePage from "./pages/movie/moviePage.jsx";
+
+import RootLayout from "./layout/root-layout.jsx"
+import Movieslayout from "./layout/movies-layout.jsx";
+
+import './App.css';
+import './font.css';
+
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <RootLayout/>,
+      errorElement: <NotFound/>,
+      children: [
+        {
+          index: true,
+          element: <HomePage/>
+        },
+        {
+          path: 'login',
+          element: <LoginPage/>
+        },
+        {
+          path: 'sign-up',
+          element: <SignUpPage/>
+        },
+        {
+          path: 'search',
+          element: <SearchPage/>
+        },
+        {
+          path: 'movie-category',
+          element: <Movieslayout/>,
+          children: [
+            {
+              index: true,
+              element: <MovieCategoryPage/>
+            },
+            {
+              path: ':category',
+              element: <MoviesPage/>,
+            },
+          ]
+        }, 
+        {
+          path: 'moviePage/:movieId',
+          element: <MoviePage/>
+        },
+        {
+          path: '*',
+          element: <NotFound/>
+        }, 
+      ]
+    }
+  ])
+
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
