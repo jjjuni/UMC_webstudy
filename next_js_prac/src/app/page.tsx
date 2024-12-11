32c3ca2ed0f3e143f67b01724fc047c8768c206b
jjjuni <0210leejun@gmail.com>
2024-12-11 12:52:45 +0900

    Next.js 실습

--- next_js_prac/src/app/page.tsx
+++ next_js_prac/src/app/page.tsx
@@ -4,24 +4,32 @@ import style from './page.module.css';
 
 export default async function Home() {
   
-  const getMovies = async () => {
-    try {
-      const response = await axiosTMDBInstance.get(`/movie/now_playing?language=ko-KR&page=1`);
-      return response.data; // API 응답 데이터 반환
-    } catch (error) {
-      console.log("Error fetching movies:", error);
-      return { results: [] }; // 기본값 반환
-    }
-  };
+  // const getMovies = async () => {
+  //   try {
+  //     const response = await axiosTMDBInstance.get(`/movie/now_playing?language=ko-KR&page=1`);
+  //     return response.data; // API 응답 데이터 반환
+  //   } catch (error) {
+  //     console.log("Error fetching movies:", error);
+  //     return { results: [] }; // 기본값 반환
+  //   }
+  // };
 
-  const movies = await getMovies();
+  // const movies = await getMovies();
+
+  const data = await fetch(`${process.env.VITE_MOVIE_API_URL}/movie/now_playing?language=ko-KR&page=1`, {
+    headers: {
+      Authorization: `Bearer ${process.env.VITE_TMDB_TOKEN}`,
+    },
+  })
+  const movies = await data.json()
+
   
   return (
     <div className="flex flex-col pt-20 justify-center items-center">
-      <h1 className={style.title}>요즘 뜨는 영화</h1>
+      <h1 className={style.title}>상영 중인 영화</h1>
       <div className={style.container}>
         <div className={style.posterWrapper}>
-          {movies.results.map((movie: { id: number; title: string }) => (
+          {movies?.results?.map((movie: { id: number; title: string }) => (
             <Poster key={movie.id} movie={movie}/>
           ))}
         </div>
