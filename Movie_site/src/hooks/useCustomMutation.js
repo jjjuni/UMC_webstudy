a94818511527b95eb306027c1d2564df67b6dfd1
jjjuni <0210leejun@gmail.com>
2025-01-21 22:22:27 +0900

    ✏️ 오타 수정

--- Movie_site/src/hooks/useCustomMutation.js
+++ Movie_site/src/hooks/useCustomMutation.js
@@ -1,7 +1,7 @@
 import { useMutation } from "@tanstack/react-query"
 import { axiosLOGInstance } from "../apis/axios-instance";
 
-const useCustiomMutation = () => {
+const useCustomMutation = () => {
   const { mutateAsync } = useMutation({
     mutationFn: async ({url, data}) => {
       const response = await axiosLOGInstance.post(url, data)
@@ -17,4 +17,4 @@ const useCustiomMutation = () => {
   return mutateAsync;
 }
 
-export default useCustiomMutation;
+export default useCustomMutation;
